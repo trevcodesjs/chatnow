0e41b1dc7185467c7fab9a270c0a638f2e22211c
cs136 <cs136@cs136-desktop.(none)>
2012-02-23 06:03:22 -0500

    test

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
-app.listen(8080);
-
-function handler (req, res) {
-   fs.readFile(__dirname + '/index.html',
-      function (err, data) {
-         if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-         }
-         res.writeHead(200);
-         res.end(data);
-      }
-   );
-}
-
-io.set('log level', 1);
-io.sockets.on('connection', function (socket) {
-   socket.emit('id', {id : socket.id});
-   socket.on('msg', function (data) {
-      socket.broadcast.emit('msg', data);
-  });
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\nApp (9999999999999) is running..');
+}).listen(14897);
