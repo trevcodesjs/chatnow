305f47995818bd819b65a92cdf52d68e969b6b08
cs136 <cs136@cs136-desktop.(none)>
2012-02-23 05:55:31 -0500

    adding my files

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (chatnow) is running..');
-}).listen(14841);
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8080);
+
+function handler (req, res) {
+   fs.readFile(__dirname + '/index.html',
+      function (err, data) {
+         if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+         }
+         res.writeHead(200);
+         res.end(data);
+      }
+   );
+}
+
+io.set('log level', 1);
+io.sockets.on('connection', function (socket) {
+   socket.emit('id', {id : socket.id});
+   socket.on('msg', function (data) {
+      socket.broadcast.emit('msg', data);
+  });
+});
